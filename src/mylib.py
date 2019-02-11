16c38751bc2a67abaf813ac154fcc0960338d793
viktor <deevictor@gmail.com>
2019-02-11 21:03:17 +1000

    wip1

---
+++ src/mylib.py
@@ -0,0 +1,35 @@
+from requests import get
+from bs4 import BeautifulSoup
+
+
+def st(tag):
+    return tag.text.strip()
+
+
+def pull_prices(company):
+    url = f"https://www.nasdaq.com/symbol/{company}/historical"
+
+    response = get(url)
+    # print(response.text[:1000])
+    if response.status_code == 200:
+        html_soup = BeautifulSoup(response.text, 'html.parser')
+        title = html_soup.find('h1')
+        # if html_soup.find(id="historicalContainer"):
+        # lines = html_soup.find_all('')
+        try:
+            trs_soup = html_soup.find(id="historicalContainer").find("tbody").find_all(
+                'tr')
+            # print(len(chunk))
+            rows = []
+            for row in trs_soup:
+                row = row.find_all('td')
+                rows.append({
+                    'Date': st(row[0]), 'Open': st(row[1]),
+                    'High': st(row[2]), 'Low': st(row[3]),
+                    'Close/Last': st(row[3]), 'Volume': st(row[4]),
+                })
+        except AttributeError as e:
+            raise Exception(
+                'The table is not found, markup has changed: {}'.format(e))
+
+        return rows
