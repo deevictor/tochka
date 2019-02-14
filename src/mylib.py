5bd84cbe6eae6ae1d8a7c5e55a5e9c694aa2b5c8
viktor <deevictor@gmail.com>
2019-02-14 15:32:36 +1000

    parsing of prices and insider trades implemented

--- src/mylib.py
+++ src/mylib.py
@@ -1,35 +1,95 @@
 from requests import get
 from bs4 import BeautifulSoup
+from multiprocessing import Pool
+
+HEADERS = {
+    'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 '
+                  '(HTML, like Gecko) Chrome/72.0.3626.81 Safari/537.36'
+}
 
 
-def st(tag):
-    return tag.text.strip()
-
-
-def pull_prices(company):
-    url = f"https://www.nasdaq.com/symbol/{company}/historical"
-
-    response = get(url)
-    # print(response.text[:1000])
+def parse_table(url, rows_selector):
+    response = get(url, headers=HEADERS, timeout=15)
     if response.status_code == 200:
         html_soup = BeautifulSoup(response.text, 'html.parser')
-        title = html_soup.find('h1')
-        # if html_soup.find(id="historicalContainer"):
-        # lines = html_soup.find_all('')
         try:
-            trs_soup = html_soup.find(id="historicalContainer").find("tbody").find_all(
-                'tr')
-            # print(len(chunk))
-            rows = []
-            for row in trs_soup:
-                row = row.find_all('td')
-                rows.append({
-                    'Date': st(row[0]), 'Open': st(row[1]),
-                    'High': st(row[2]), 'Low': st(row[3]),
-                    'Close/Last': st(row[3]), 'Volume': st(row[4]),
-                })
+            rows_soup = html_soup.select(rows_selector)
+            rows_list = [[
+                v.text.strip() for v in row.select('td')
+            ] for row in rows_soup]
         except AttributeError as e:
             raise Exception(
-                'The table is not found, markup has changed: {}'.format(e))
+                'The table is not found, markup has changed: {}'.format(e)
+            )
+        except ConnectionError as e:
+            raise Exception(
+                'Make sure you are connected to Internet: {}'.format(e)
+            )
+        except TimeoutError as e:
+            raise Exception(
+                'Timeout Error: {}'.format(e)
+            )
 
-        return rows
+        return rows_list
+
+
+def get_prices(company):
+    url = f"https://www.nasdaq.com/symbol/{company}/historical"
+    selector = 'div.genTable table tbody tr'
+    rows_list = parse_table(url, selector)
+
+    return {company: rows_list}
+
+
+def parse_prices(companies, n):
+    with Pool(n) as p:
+        prices = p.map(get_prices, companies)
+    return prices
+
+
+def get_pages(company):
+    url = f"https://www.nasdaq.com/symbol/{company}/insider-trades"
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 '
+                      '(KHTML, like Gecko) Chrome/72.0.3626.81 Safari/537.36'
+    }
+    response = get(url, headers=headers, timeout=10)
+    if response.status_code == 200:
+        html_soup = BeautifulSoup(response.text, 'html.parser')
+        try:
+            # lets find out the number of the last page
+            last_page_url = html_soup.find(
+                'a', id='quotes_content_left_lb_LastPage'
+            )['href']
+            last_page_number = int(last_page_url[last_page_url.index('=')+1:])
+            last_page_taken = last_page_number if last_page_number < 10 else 10
+            pages = [url+f'?page={n}' for n in range(1, last_page_taken+1)]
+        except AttributeError as e:
+            raise Exception(
+                'The table is not found, markup has changed: {}'.format(e)
+            )
+        except ConnectionError as e:
+            raise Exception(
+                'Make sure you are connected to Internet: {}'.format(e)
+            )
+        except TimeoutError as e:
+            raise Exception(
+                'Timeout Error: {}'.format(e)
+            )
+
+        return pages
+
+
+def get_insider_trades(company):
+    rows_selector = 'div.genTable table thead ~ tr'
+    pages = get_pages(company)
+    table = [parse_table(page, rows_selector) for page in pages]
+
+    return {company: table}
+
+
+def parse_insider_trades(companies, n):
+    with Pool(n) as p:
+        insider_trades = p.map(get_insider_trades, companies)
+
+    return insider_trades
--- src/scraper.py
+++ src/scraper.py
@@ -1,12 +1,13 @@
-from requests import get
-from bs4 import BeautifulSoup
+import time
 
-company = 'aapl'
-url = f"https://www.nasdaq.com/symbol/{company}/historical"
+from mylib import parse_insider_trades, parse_prices
 
-response = get(url)
-# print(response.text[:1000])
-html_soup = BeautifulSoup(response.text, 'html.parser')
-# lines = html_soup.find_all('')
-chunk = html_soup.find(id="historicalContainer").find("tbody").find_all('tr')
-print(type(chunk))
+
+# companies = ['aapl', 'cvx', 'goog']
+companies = ['aapl', ]
+t = time.process_time()
+insider_trades_data = parse_insider_trades(companies, 10)
+prices_data = parse_prices(companies, 10)
+elapsed_time = time.process_time() - t
+print(elapsed_time)
+print('DONE')
